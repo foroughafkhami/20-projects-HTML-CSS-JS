1c081ac2e38e7789b2fe4e7f6e9a3601533412d2
foroughafkhami <foroughfarnaz80@gmail.com>
2025-01-21 18:18:20 +0330

    styling completed

---
+++ quote-generator/script.js
@@ -0,0 +1,66 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const xBtn = document.getElementById("x");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+// Get Quotes from API
+let apiQuotes = [];
+// Show new Quote
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+function newQuote() {
+  loading();
+  // pick a random quote from apiQuotes array
+  const quote =
+    apiQuotes.quotes[Math.floor(Math.random() * apiQuotes.quotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //Check Quote length to detemine styling
+  if (quote.quote.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set Quote,hide loader
+  quoteText.textContent = quote.quote;
+  complete();
+}
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://dummyjson.com/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+
+    newQuote();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}
+  `;
+  window.open(twitterUrl, "_blank");
+}
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+xBtn.addEventListener("click", tweetQuote);
+// On load
+getQuotes();
