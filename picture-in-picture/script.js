fc58acde45a202d1cbcb671d0c4fe05b1a647ed1
foroughafkhami <foroughfarnaz80@gmail.com>
2025-01-22 19:58:41 +0330

    picture in picture functionality added

---
+++ picture-in-picture/script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream,pass to video element,then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error here
+    console.log("Whoops,error here:", error);
+  }
+}
+button.addEventListener("click", async () => {
+  // Disable the buttob
+  button.disabled = true;
+  // start picture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset the button
+  button.disabled = false;
+});
+//On load
+
+selectMediaStream();
