73c9fa28210628f7bac8012674d4689b96eac89e
foroughafkhami <foroughfarnaz80@gmail.com>
2025-01-26 20:29:17 +0330

    some improvements

--- joke-teller/script.js
+++ joke-teller/script.js
@@ -5,25 +5,26 @@ async function createJoke() {
   try {
     const response = await fetch(jokeApiUrl);
     joke = await response.json();
-    console.log(joke.setup + joke.delivery);
-
-    return joke.setup + joke.delivery;
+    if (joke.setup) {
+      return joke.setup + joke.delivery;
+    } else {
+      return joke.joke;
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
 async function readJoke() {
-  // if ("speedSynthesis" in window) {
-  let joke = new SpeechSynthesisUtterance();
-  const text = await createJoke();
-  joke.text = text ? text : "Try Again";
+  try {
+    let joke = new SpeechSynthesisUtterance();
+    const text = await createJoke();
+    joke.text = text ? text : "Try Again";
 
-  window.speechSynthesis.speak(joke);
-
-  // } else {
-  //   alert("Sorry, your browser doesn't support text to speech!");
-  // }
+    window.speechSynthesis.speak(joke);
+  } catch (error) {
+    console.log("whoops", error);
+  }
 }
 
 button.addEventListener("click", readJoke);
