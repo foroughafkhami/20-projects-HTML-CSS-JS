6678d86d7039d029b6459761fe8ed26f4303a850
foroughafkhami <foroughfarnaz80@gmail.com>
2025-01-27 15:50:42 +0330

    save theme in local storage

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -31,11 +31,23 @@ function switchTheme(event) {
   console.log(event.target.checked);
   if (event.target.checked === true) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+
     lightMode();
   }
 }
 // Event listerner
 toggleSwitch.addEventListener("change", switchTheme);
+//Check local Storage for theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
